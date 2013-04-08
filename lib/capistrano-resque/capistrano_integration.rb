56430ca66dd2441ee35cdca96e2fac0e20fe0f1f
Corey Martella <cmartella@zynga.com>
2013-04-08 14:35:47 -0400

    allow env vars

--- lib/capistrano-resque/capistrano_integration.rb
+++ lib/capistrano-resque/capistrano_integration.rb
@@ -34,7 +34,7 @@ module CapistranoResque
 
         def start_command(queue, pid)
           "cd #{current_path}; #{try_sudo :as => fetch(:resque_runner,user)} RAILS_ENV=#{rails_env} QUEUE=\"#{queue}\" \
-           PIDFILE=#{pid} BACKGROUND=yes VERBOSE=1 INTERVAL=#{interval} \
+           PIDFILE=#{pid} #{fetch(:workers_env,{})[queue]} BACKGROUND=yes VERBOSE=1 INTERVAL=#{interval} \
            bundle exec rake resque:work"
         end
 
